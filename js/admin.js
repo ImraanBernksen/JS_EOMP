3d2663000cb2933b92a2195268681fed0b4a66e6
ImraanBernksen <imraanbernksen@gmail.com>
2022-12-08 12:25:43 +0200

    yep

--- js/admin.js
+++ js/admin.js
@@ -1,114 +1,181 @@
-let products = JSON.parse(localStorage.getItem('products ')) ?
-JSON.parse(localStorage.getItem('products ')) : [
-    {
-        id: 1,
-        productName: "Aural Series",
-        productDescription: "TWS BT Over-The-Ear Headphones",
-        productQuantity: 40,
-        productPrice: 300
-    },
-    {
-        id: 2,
-        productName: "Silenco Series",
-        productDescription: "Noise Cancelling BT Headphones",
-        productQuantity: 40,
-        productPrice: 1300
-    },
-    {
-        id: 3,
-        productName: "Sonata Series",
-        productDescription: "Noise Cancelling Headphones",
-        productQuantity: 40,
-        productPrice: 800
-    },
-    {
-        id: 4,
-        productName: "Taurus Series",
-        productDescription: "True Wireless Stereo Earbuds",
-        productQuantity: 40,
-        productPrice: 500
-    },
-    {
-        id: 5,
-        productName: "Ore Series",
-        productDescription: "True Wireless Earphones",
-        productQuantity: 40,
-        productPrice: 600
-    },
-    {
-        id: 6,
-        productName: "Jonagold Series",
-        productDescription: "iPhone Compatible Earphones",
-        productQuantity: 40,
-        productPrice: 500
-    },
-    {
-        id: 7,
-        productName: "Mamba Seriesr",
-        productDescription: "Portable Bluetooth Speake",
-        productQuantity: 30,
-        productPrice: 400
-    },
-    {
-        id: 8,
-        productName: "Gemini Series",
-        productDescription: "TWS Twin Pack Mini Speakers",
-        productQuantity: 30,
-        productPrice: 500
-    },
-    {
-        id: 9,
-        productName: "Adder Series",
-        productDescription: "Waterproof Bluetooth Speaker",
-        productQuantity: 30,
-        productPrice: 1500
-    },
-    {
-        id: 10,
-        productName: "Weave Series",
-        productDescription: "1.2m, 4-In-1 Cable",
-        productQuantity: 50,
-        productPrice: 200
-    },
-    {
-        id: 11,
-        productName: "Iron Series",
-        productDescription: "USB Type-C to USB-C Cable",
-        productQuantity: 50,
-        productPrice: 150
-    },
-    {
-        id: 12,
-        productName: "Iron Series",
-        productDescription: "1.2m, 3-in-1 Cable",
-        productQuantity: 50,
-        productPrice: 200
-    },
-];
+let products;
+if (localStorage.getItem("products")) {
+    products = localStorage.getItem("products")
+} else {
+    products = [
+        {
+            id: 1,
+            productImage: "/images/headphones1.jpg",
+            productName: "Aural Series",
+            productDescription: "TWS BT Over-The-Ear Headphones",
+            productQuantity: 40,
+            productPrice: 300
+        },
+        {
+            id: 2,
+            productImage: "/images/headphones2.jpg",
+            productName: "Silenco Series",
+            productDescription: "Noise Cancelling BT Headphones",
+            productQuantity: 40,
+            productPrice: 1300
+        },
+        {
+            id: 3,
+            productImage: "/images/headphones3.jpg",
+            productName: "Sonata Series",
+            productDescription: "Noise Cancelling Headphones",
+            productQuantity: 40,
+            productPrice: 800
+        },
+        {
+            id: 4,
+            productImage: "/images/earphones1.jpg",
+            productName: "Taurus Series",
+            productDescription: "True Wireless Stereo Earbuds",
+            productQuantity: 40,
+            productPrice: 500
+        },
+        {
+            id: 5,
+            productImage: "/images/earphones2.jpg",
+            productName: "Ore Series",
+            productDescription: "True Wireless Earphones",
+            productQuantity: 40,
+            productPrice: 600
+        },
+        {
+            id: 6,
+            productImage: "/images/earphones3.jpg",
+            productName: "Jonagold Series",
+            productDescription: "iPhone Compatible Earphones",
+            productQuantity: 40,
+            productPrice: 500
+        },
+        {
+            id: 7,
+            productImage: "/images/speakers1.jpg",
+            productName: "Mamba Seriesr",
+            productDescription: "Portable Bluetooth Speake",
+            productQuantity: 30,
+            productPrice: 400
+        },
+        {
+            id: 8,
+            productImage: "/images/speakers2.jpg",
+            productName: "Gemini Series",
+            productDescription: "TWS Twin Pack Mini Speakers",
+            productQuantity: 30,
+            productPrice: 500
+        },
+        {
+            id: 9,
+            productImage: "/images/speakers3.jpg",
+            productName: "Adder Series",
+            productDescription: "Waterproof Bluetooth Speaker",
+            productQuantity: 30,
+            productPrice: 1500
+        },
+        {
+            id: 10,
+            productImage: "/images/cable1.jpg",
+            productName: "Weave Series",
+            productDescription: "1.2m, 4-In-1 Cable",
+            productQuantity: 50,
+            productPrice: 200
+        },
+        {
+            id: 11,
+            productImage: "/images/cable2.jpg",
+            productName: "Iron Series",
+            productDescription: "USB Type-C to USB-C Cable",
+            productQuantity: 50,
+            productPrice: 150
+        },
+        {
+            id: 12,
+            productImage: "/images/cable3.jpg",
+            productName: "Iron Series",
+            productDescription: "1.2m, 3-in-1 Cable",
+            productQuantity: 50,
+            productPrice: 200
+        },
+    ]
+    localStorage.setItem('products', JSON.stringify(products))
+}
 
 function displayProducts(){
+    products = JSON.parse(localStorage.getItem('products'))
+
     let tbody = document.querySelector('tbody');
-    Object.keys(products).forEach( (item)=>{
-        if(products.length){
-            console.log(`${item}: ${products[item]}`);
+    products.forEach( (item)=>{
+            console.log(`${item}: ${item}`);
             tbody.innerHTML +=
             `
             <tr>
-            <td>${products[item].id}</td>
-            <td>${products[item].productName}</td>
-            <td>${products[item].productDescription}</td>
-            <td>${products[item].productQuantity}</td>
-            <td>R${products[item].productPrice}</td>
+            <td>${item.id}</td>
+            <td>${item.productName}</td>
+            <td>${item.productDescription}</td>
+            <td>${item.productQuantity}</td>
+            <td>R${item.productPrice}</td>
             <td><button>Edit</button> <button onclick="deleteProduct">Delete</button></td>
             </tr>
             `
-        }
     })
 }
 
 displayProducts()
 
-function deleteProduct() {
-    
+var tableBody =  document.querySelector('#tbody_')
+
+var row = null;
+function submitProduct() {
+  var data = getData();
+  var readData = readingDataFLS(data);
+  if (data == false) {
+} else {
+    if (row == null) {
+     insert(readData);
+    } else {
+     updateProducts();
+    }
+  }
+}
+function getData() {
+  var name = document.getElementById("name").value;
+  var description = document.getElementById("description").value;
+  var quantity = document.getElementById("quantity").value;
+  var price = document.getElementById("price").value;
+  var list = [name, description, quantity , price];
+  if (list.includes("")) {
+      return false;
+  } else {
+    return list;
+  }
+}
+function insert(readData) {
+ var row = tableBody.insertRow();
+ row.insertCell(0).innerHTML = readData[0];
+ row.insertCell(1).innerHTML = readData[1];
+ row.insertCell(2).innerHTML = readData[2];
+ row.insertCell(3).innerHTML = readData[3];
+ row.insertCell(4).innerHTML = `<button onclick = editProducts(this)>Edit</button><button onclick = removeProducts(this)>Delete</button>`;
+}
+function editProducts(info) {
+ row = info.parentElement.parentElement;
+ document.getElementById("name").value = row.cells[0].innerHTML;
+ document.getElementById("description").value = row.cells[1].innerHTML;
+ document.getElementById("quantity").value = row.cells[2].innerHTML;
+ document.getElementById("price").value = row.cells[3].innerHTML;
+}
+function updateProducts() {
+ row.cells[0].innerHTML = document.getElementById("name").value;
+ row.cells[1].innerHTML = document.getElementById("description").value;
+ row.cells[2].innerHTML = document.getElementById("quantity").value;
+ row.cells[3].innerHTML = document.getElementById("price").value;
+ row = null;
+}
+function removeProducts(info) {
+ row = info.parentElement.parentElement;
+ document.getElementById("tbody_").deleteRow(row.rowIndex);
 }
 
--- js/product.js
+++ js/product.js
@@ -1,103 +1,122 @@
-let products = JSON.parse(localStorage.getItem('products')) ?
-JSON.parse(localStorage.getItem('products')) : [
-    {
-        id: 1,
-        productImage: "/images/headphones1.jpg",
-        productName: "Aural Series",
-        productDescription: "TWS BT Over-The-Ear Headphones",
-        productPrice: 300
-    },
-    {
-        id: 2,
-        productImage: "/images/headphones2.jpg",
-        productName: "Silenco Series",
-        productDescription: "Noise Cancelling BT Headphones",
-        productPrice: 1300
-    },
-    {
-        id: 3,
-        productImage: "/images/headphones3.jpg",
-        productName: "Sonata Series",
-        productDescription: "Noise Cancelling Headphones",
-        productPrice: 800
-    },
-    {
-        id: 4,
-        productImage: "/images/earphones1.jpg",
-        productName: "Taurus Series",
-        productDescription: "True Wireless Stereo Earbuds",
-        productPrice: 500
-    },
-    {
-        id: 5,
-        productImage: "/images/earphones2.jpg",
-        productName: "Ore Series",
-        productDescription: "True Wireless Earphones",
-        productPrice: 600
-    },
-    {
-        id: 6,
-        productImage: "/images/earphones3.jpg",
-        productName: "Jonagold Series",
-        productDescription: "iPhone Compatible Earphones",
-        productPrice: 500
-    },
-    {
-        id: 7,
-        productImage: "/images/speakers1.jpg",
-        productName: "Mamba Seriesr",
-        productDescription: "Portable Bluetooth Speake",
-        productPrice: 400
-    },
-    {
-        id: 8,
-        productImage: "/images/speakers2.jpg",
-        productName: "Gemini Series",
-        productDescription: "TWS Twin Pack Mini Speakers",
-        productPrice: 500
-    },
-    {
-        id: 9,
-        productImage: "/images/speakers3.jpg",
-        productName: "Adder Series",
-        productDescription: "Waterproof Bluetooth Speaker",
-        productPrice: 1500
-    },
-    {
-        id: 10,
-        productImage: "/images/cable1.jpg",
-        productName: "Weave Series",
-        productDescription: "1.2m, 4-In-1 Cable",
-        productPrice: 200
-    },
-    {
-        id: 11,
-        productImage: "/images/cable2.jpg",
-        productName: "Iron Series",
-        productDescription: "USB Type-C to USB-C Cable",
-        productPrice: 150
-    },
-    {
-        id: 12,
-        productImage: "/images/cable3.jpg",
-        productName: "Iron Series",
-        productDescription: "1.2m, 3-in-1 Cable",
-        productPrice: 200
-    },
-];
+let products;
+if (localStorage.getItem("products")) {
+    products = localStorage.getItem("products")
+} else {
+    products = [
+        {
+            id: 1,
+            productImage: "/images/headphones1.jpg",
+            productName: "Aural Series",
+            productDescription: "TWS BT Over-The-Ear Headphones",
+            productQuantity: 40,
+            productPrice: 300
+        },
+        {
+            id: 2,
+            productImage: "/images/headphones2.jpg",
+            productName: "Silenco Series",
+            productDescription: "Noise Cancelling BT Headphones",
+            productQuantity: 40,
+            productPrice: 1300
+        },
+        {
+            id: 3,
+            productImage: "/images/headphones3.jpg",
+            productName: "Sonata Series",
+            productDescription: "Noise Cancelling Headphones",
+            productQuantity: 40,
+            productPrice: 800
+        },
+        {
+            id: 4,
+            productImage: "/images/earphones1.jpg",
+            productName: "Taurus Series",
+            productDescription: "True Wireless Stereo Earbuds",
+            productQuantity: 40,
+            productPrice: 500
+        },
+        {
+            id: 5,
+            productImage: "/images/earphones2.jpg",
+            productName: "Ore Series",
+            productDescription: "True Wireless Earphones",
+            productQuantity: 40,
+            productPrice: 600
+        },
+        {
+            id: 6,
+            productImage: "/images/earphones3.jpg",
+            productName: "Jonagold Series",
+            productDescription: "iPhone Compatible Earphones",
+            productQuantity: 40,
+            productPrice: 500
+        },
+        {
+            id: 7,
+            productImage: "/images/speakers1.jpg",
+            productName: "Mamba Seriesr",
+            productDescription: "Portable Bluetooth Speake",
+            productQuantity: 30,
+            productPrice: 400
+        },
+        {
+            id: 8,
+            productImage: "/images/speakers2.jpg",
+            productName: "Gemini Series",
+            productDescription: "TWS Twin Pack Mini Speakers",
+            productQuantity: 30,
+            productPrice: 500
+        },
+        {
+            id: 9,
+            productImage: "/images/speakers3.jpg",
+            productName: "Adder Series",
+            productDescription: "Waterproof Bluetooth Speaker",
+            productQuantity: 30,
+            productPrice: 1500
+        },
+        {
+            id: 10,
+            productImage: "/images/cable1.jpg",
+            productName: "Weave Series",
+            productDescription: "1.2m, 4-In-1 Cable",
+            productQuantity: 50,
+            productPrice: 200
+        },
+        {
+            id: 11,
+            productImage: "/images/cable2.jpg",
+            productName: "Iron Series",
+            productDescription: "USB Type-C to USB-C Cable",
+            productQuantity: 50,
+            productPrice: 150
+        },
+        {
+            id: 12,
+            productImage: "/images/cable3.jpg",
+            productName: "Iron Series",
+            productDescription: "1.2m, 3-in-1 Cable",
+            productQuantity: 50,
+            productPrice: 200
+        },
+    ]
+    localStorage.setItem('products', JSON.stringify(products))
+}
 
 function displayProducts(){
+    products = JSON.parse(localStorage.getItem('products'))
+
     let tbody = document.querySelector('#items_cards');
-    Object.keys(products).forEach( (item)=>{
+    products.forEach( (item)=>{
         if(products.length){
-            console.log(`${item}: ${products[item]}`);
+            console.log(`${item}: ${item}`);
             tbody.innerHTML +=
             `
             <div class="card col-4">
-            <img src="${products[item].productImage}" class="card-img-top">
+            <img src="${item.productImage}" class="card-img-top">
             <div class="card-body">
-            <h5 class="card-title">${products[item].productName} - ${products[item].productDescription}</h5>
-            <p class="card-text text-center">R${products[item].productPrice} <button class="bg-dark text-white">Checkout</button></p>
+            <h5 class="card-title">${item.productName} - ${item.productDescription}</h5>
+            <p class="card-text text-center">R${item.productPrice} <a href="../html/checkout.html" class="bg-dark text-white" id="tick">Checkout</a></p>
             </div>
             </div>
             `
@@ -108,5 +127,3 @@ function displayProducts(){
 displayProducts()
 
 localStorage.setItem('products', JSON.stringify(products));
-
-let prods = JSON.parse(localStorage.getItem('products'))
