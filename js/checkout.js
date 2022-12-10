03eab924983a25ece43315aa8f173ec26642b3b8
ImraanBernksen <imraanbernksen@gmail.com>
2022-12-10 15:20:09 +0200

    finished

--- js/checkout.js
+++ js/checkout.js
@@ -151,48 +151,4 @@ localStorage.setItem('products', JSON.stringify(products));
 /*Alert*/
 function buyAlert() {
     alert("Thank you for shopping");
-  }
-
-/* Sorting Products */
-function sortProduct() {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("table_checkout");
-    switching = true;
-    while (switching) {
-      switching = false;
-      rows = table.rows;
-      for (i = 1; i < (rows.length - 1); i++) {
-        shouldSwitch = false;
-        x = rows[i].getElementsByTagName("TD")[2];
-        y = rows[i + 1].getElementsByTagName("TD")[2];
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-      if (shouldSwitch) {
-        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-        switching = true;
-      }
-    }
-}
-
-/* Filtering Search */
-function searchProduct() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("table_checkout");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[2];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
 }
